bbf626597da7681928797d6daa57f07d777b6ca7
Julian Winter <julian.winter91@gmail.com>
2024-10-20 00:19:06 +0200

    added basic project information

--- README.md
+++ README.md
@@ -1,28 +1,21 @@
 # SuperbCalendar
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.2.0.
-`json-server --watch db.json`
+A simple exemplary calendar application - have fun!
 
-## Development server
+### Setup
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The application will automatically reload if you change any of the source files.
+1. Run `npm i` to install all necessary packages for this application.
+2. Run `node generator.js` to create some exemplary appointments for your calendar. In case an error occurs, the step can be skipped. It is merely nice-to-have.
+3. Run `json-server --watch db.json` to start the json-server (before yout start the application itself!).
+4. Run `ng s` to start the SuperbCalendar
 
-## Code scaffolding
+### Q&A
 
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
-
-## Build
-
-Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory.
-
-## Running unit tests
-
-Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.io).
-
-## Running end-to-end tests
-
-Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To use this command, you need to first add a package that implements end-to-end testing capabilities.
-
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.io/cli) page.
+1. What is the json-server?
+   - A simple lightweight fake backend that is using a json file as database. Further information on https://github.com/typicode/json-server.
+2. What is the db.json?
+   - The db.json is a fake database in form of a json file. It is part of the json-server.
+3. What is the generator.js?
+   - It is a vanilla JavaScript file for generating some exemplary appointments for this app. If this script cannot be run, there is already a db.json, but an empty one.
+4. Is there any testing?
+   - Currently, there is no testing in this app.
---
+++ generator.js
@@ -0,0 +1,190 @@
+let today = new Date();
+
+function addDays(date, days) {
+  var result = new Date(date);
+  result.setDate(result.getDate() + days);
+  return result;
+}
+
+let monday = addDays(today, -today.getDay()+1);
+let data = {
+  "appointments": []
+};
+let desiredWeekDay = [0, 0, 0, 1, 1, 2, 2, 3, 4, 4]
+
+let rawAppointments = [
+  {
+    "title": "Conversation with Konstantin",
+    "startTime": {
+      "hour": 8,
+      "minute": 30
+    },
+    "endTime": {
+      "hour": 10,
+      "minute": 0
+    },
+    "start": null,
+    "end": null,
+    "details": "About the current situation in the team",
+    "id": 1
+  },
+  {
+    "title": "Database problems: Ideas exchange with team",
+    "startTime": {
+      "hour": 11,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 12,
+      "minute": 0
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 2
+  },
+  {
+    "title": "Lunch break with Miriam",
+    "startTime": {
+      "hour": 12,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 13,
+      "minute": 0
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 3
+  },
+  {
+    "title": "Code review junior engineer",
+    "startTime": {
+      "hour": 9,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 10,
+      "minute": 0
+    },
+    "start": null,
+    "end": null,
+    "details": "Feedback about the calendar component; focussing on the missing unsubscribe()",
+    "id": 4
+  },
+  {
+    "title": "Call with munich team",
+    "startTime": {
+      "hour": 16,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 17,
+      "minute": 20
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 5
+  },
+  {
+    "title": "Refinement",
+    "startTime": {
+      "hour": 11,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 11,
+      "minute": 40
+    },
+    "start": null,
+    "end": null,
+    "details": "Focussing on interactive calendar events (ticket-14021)",
+    "id": 6
+  },
+  {
+    "title": "Scrum feedback to Lisa",
+    "startTime": {
+      "hour": 13,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 13,
+      "minute": 20
+    },
+    "start": null,
+    "end": null,
+    "details": "Lisa asked for feedback for her first meeting as a scrum master",
+    "id": 7
+  },
+  {
+    "title": "NGRX workshop",
+    "startTime": {
+      "hour": 9,
+      "minute": 0
+    },
+    "endTime": {
+      "hour": 12,
+      "minute": 15
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 8
+  },
+  {
+    "title": "workshop results presentation",
+    "startTime": {
+      "hour": 11,
+      "minute": 30
+    },
+    "endTime": {
+      "hour": 12,
+      "minute": 30
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 9
+  },
+  {
+    "title": "Driving to Munich",
+    "startTime": {
+      "hour": 15,
+      "minute": 30
+    },
+    "endTime": {
+      "hour": 17,
+      "minute": 30
+    },
+    "start": null,
+    "end": null,
+    "details": undefined,
+    "id": 9
+  }
+]
+
+rawAppointments.forEach((appointment, index) => {
+  let currentStart = addDays(monday, desiredWeekDay[index] ? desiredWeekDay[index] : 0);
+  let currentEnd = addDays(monday, desiredWeekDay[index] ? desiredWeekDay[index] : 0);
+
+  currentStart.setHours(appointment.startTime.hour);
+  currentStart.setMinutes(appointment.startTime.minute);
+  appointment.start = currentStart;
+
+  currentEnd.setHours(appointment.endTime.hour);
+  currentEnd.setMinutes(appointment.endTime.minute);
+  appointment.end = currentEnd;
+
+  appointment.id = index;
+
+  data.appointments.push(appointment);
+})
+
+let jsonData = JSON.stringify(data);
+
+let fs = require('fs');
+fs.writeFile("db.json", jsonData, function(err) {
+  if(err) console.log('error', err);
+});
